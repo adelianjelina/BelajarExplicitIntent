fbc0cb183a3ab79c5a871d51e690536cd342ad14
adelianjelina <114387035+adelianjelina@users.noreply.github.com>
2022-10-10 15:36:35 +0700

    Belajar Explicit Intent

---
+++ app/src/main/java/com/adelianjelina/belajarexplicitintent/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.adelianjelina.belajarexplicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView tvNama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setTitle("layout B");
+
+        tvNama = findViewById(R.id.tv_nama);
+
+        Intent terima = getIntent();
+        String yNama = terima.getStringExtra("xNama");
+        tvNama.setText(yNama);
+
+
+    }
+}
